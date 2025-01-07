d204e6bb29e3480ba91f7cfb755b00caad39435e
jacksonrodrigodev <jackson_techno2@hotmail.com>
2025-01-07 16:36:52 -0300

    format pep 8 with black

--- scripts/load_data_mysql.py
+++ scripts/load_data_mysql.py
@@ -5,28 +5,30 @@ import os
 
 load_dotenv
 
+
 def connect_mysql(host_name, user_name, pw):
-    cnx = mysql.connector.connect(
-        host=host_name,
-        user=user_name,
-        password=pw
-    )
+    cnx = mysql.connector.connect(host=host_name, user=user_name, password=pw)
     return cnx
+
 
 def create_cursor(cnx):
     return cnx.cursor()
 
+
 def create_database(cursor, db_name):
     cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
+
 
 def show_databases(cursor):
     cursor.execute("SHOW DATABASES")
     for db in cursor:
         print(db)
 
+
 def create_table(cursor, db_name, query):
     cursor.execute(f"USE {db_name}")
     cursor.execute(query)
+
 
 def show_tables(cursor, db_name):
     cursor.execute(f"USE {db_name}")
@@ -34,8 +36,10 @@ def show_tables(cursor, db_name):
     for table in cursor:
         print(table)
 
+
 def read_csv(path):
     return pd.read_csv(path)
+
 
 def add_product_data(cnx, cursor, df, db_name, tb_name):
     len_columns = len(df.columns)
@@ -45,6 +49,7 @@ def add_product_data(cnx, cursor, df, db_name, tb_name):
     cursor.executemany(sql, lista_dados)
     cnx.commit()
 
+
 if __name__ == "__main__":
     # Conectando ao servidor MySQL
     host_name = os.getenv("DB_HOST")
@@ -53,7 +58,7 @@ if __name__ == "__main__":
 
     cnx = connect_mysql(host_name, user_name, pw)
     cursor = create_cursor(cnx)
-    
+
     # Criando o banco de dados e a tabela
     create_database(cursor, "dbprodutos")
     show_databases(cursor)
@@ -80,7 +85,9 @@ if __name__ == "__main__":
     show_tables(cursor, "dbprodutos")
 
     # Adicionando os dados à tabela tb_livros
-    df_livros = read_csv("/home/jackson/pipeline-python-mongo-mysql/data/tabela_livros.csv")
+    df_livros = read_csv(
+        "/home/jackson/pipeline-python-mongo-mysql/data/tabela_livros.csv"
+    )
     add_product_data(cnx, cursor, df_livros, "dbprodutos", "tb_livros")
 
     # Criando a tabela tb_produtos_2021_em_diante
@@ -106,13 +113,17 @@ if __name__ == "__main__":
     show_tables(cursor, "dbprodutos")
 
     # Adicionando os dados à tabela tb_produtos_2021_em_diante
-    df_produtos = read_csv("/home/jackson/pipeline-python-mongo-mysql/data/tabela_2021_em_diante.csv")
-    add_product_data(cnx, cursor, df_produtos, "dbprodutos", "tb_produtos_2021_em_diante")
+    df_produtos = read_csv(
+        "/home/jackson/pipeline-python-mongo-mysql/data/tabela_2021_em_diante.csv"
+    )
+    add_product_data(
+        cnx, cursor, df_produtos, "dbprodutos", "tb_produtos_2021_em_diante"
+    )
 
     cursor.execute("SELECT * FROM dbprodutos.tb_livros;")
     for row in cursor:
         print(row)
-    
+
     cursor.execute("SELECT * FROM dbprodutos.tb_produtos_2021_em_diante;")
     for row in cursor:
         print(row)
